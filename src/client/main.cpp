5f12276639fc631ff70e1ca5cd2fce1e04953314
AlexxasO <alexis.jouquan@epitech.eu>
2021-09-21 16:38:27 +0200

    pox asio

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -6,9 +6,16 @@
 */
 
 #include <iostream>
+#include <asio.hpp>
 
 int main(int ac, char *av[])
 {
-    std::cout << "Hello from client" << std::endl;
-    return 0;
+    asio::io_service io_service_object;
+    asio::ip::tcp::socket client_socket(io_service_object);
+
+    if (ac < 2) {
+        std::cerr << "Usage: ./" << av[0] << " <host>" << std::endl;
+    }
+    client_socket.connect(asio::ip::tcp::endpoint(asio::ip::address::from_string(av[1]), 9999));
+    asio::write(client_socket, asio::buffer("ptdr t ki ?"));
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -6,9 +6,41 @@
 */
 
 #include <iostream>
+#include <asio.hpp>
+#include <string>
+
+void read_it(asio::ip::tcp::socket sock);
+
+asio::streambuf b;
+
+void accept_handler(const asio::error_code& error)
+{
+    if (!error) {
+        // Accept succeeded.
+    }
+}
+
+void handle_read(const asio::error_code& error, size_t bytes_transferred) {
+    if (!error) {
+        std::istream is(&b);
+        std::string line;
+        std::getline(is, line);
+        std::cout << "line " << line << std::endl;
+    }
+}
+
+void read_it(asio::ip::tcp::socket *sock) {
+    asio::async_read_until(*sock, b, "\n", handle_read);
+}
 
 int main(int ac, char *av[])
 {
-    std::cout << "Hello from server" << std::endl;
-    return 0;
+    asio::io_service io_service_object;
+    asio::ip::tcp::acceptor acceptor_server(
+        io_service_object, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 9999));
+    asio::ip::tcp::socket server_socket(io_service_object);
+    asio::streambuf buf;
+
+    acceptor_server.accept(server_socket); // TODO: use async accept
+    read_it(&server_socket);
 }
