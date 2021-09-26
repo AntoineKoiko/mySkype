25764da70847ebd648b5024c55650bfeb9d8fd1b
AlexxasO <alexis.jouquan@epitech.eu>
2021-09-27 00:12:29 +0200

    feat(poc_asio) : asio working minimal tcp server

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,21 +1,70 @@
-/*
-** EPITECH PROJECT, 2021
-** babel
-** File description:
-** main
-*/
+// Client-side Synchronous Chatting Application
+// using C++ asio
 
-#include <iostream>
 #include <asio.hpp>
+#include <iostream>
+using namespace std;
+using namespace asio;
+using namespace asio::ip;
 
-int main(int ac, char *av[])
+string getData(tcp::socket& socket)
 {
-    asio::io_service io_service_object;
-    asio::ip::tcp::socket client_socket(io_service_object);
+    asio::streambuf buf;
+    read_until(socket, buf, "\n");
+    string data = buffer_cast<const char*>(buf.data());
+    return data;
+}
 
-    if (ac < 2) {
-        std::cerr << "Usage: ./" << av[0] << " <host>" << std::endl;
+void sendData(tcp::socket& socket, const string& message)
+{
+    write(socket,
+          buffer(message + "\n"));
+}
+
+int main(int argc, char* argv[])
+{
+    io_service io_service;
+    // socket creation
+    ip::tcp::socket client_socket(io_service);
+
+    client_socket
+        .connect(
+            tcp::endpoint(
+                address::from_string("127.0.0.1"),
+                9999));
+
+    // Getting username from user
+    cout << "Enter your name: ";
+    string u_name, reply, response;
+    getline(cin, u_name);
+
+    // Sending username to another end
+    // to initiate the conversation
+    sendData(client_socket, u_name);
+
+    // Infinite loop for chit-chat
+    while (true) {
+
+        // Fetching response
+        response = getData(client_socket);
+
+        // Popping last character "\n"
+        response.pop_back();
+
+        // Validating if the connection has to be closed
+        if (response == "exit") {
+            cout << "Connection terminated" << endl;
+            break;
+        }
+        cout << "Server: " << response << endl;
+
+        // Reading new message from input stream
+        cout << u_name << ": ";
+        getline(cin, reply);
+        sendData(client_socket, reply);
+
+        if (reply == "exit")
+            break;
     }
-    client_socket.connect(asio::ip::tcp::endpoint(asio::ip::address::from_string(av[1]), 9999));
-    asio::write(client_socket, asio::buffer("ptdr t ki ?"));
+    return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,46 +1,73 @@
-/*
-** EPITECH PROJECT, 2021
-** babel
-** File description:
-** main
-*/
+// Server-side Synchronous Chatting Application
+// using C++ asio
 
-#include <iostream>
 #include <asio.hpp>
-#include <string>
+#include <iostream>
 
-void read_it(asio::ip::tcp::socket sock);
+using namespace std;
+using namespace asio;
+using namespace asio::ip;
 
-asio::streambuf b;
-
-void accept_handler(const asio::error_code& error)
+// Driver program for receiving data from buffer
+string getData(tcp::socket& socket)
 {
-    if (!error) {
-        // Accept succeeded.
-    }
-}
-
-void handle_read(const asio::error_code& error, size_t bytes_transferred) {
-    if (!error) {
-        std::istream is(&b);
-        std::string line;
-        std::getline(is, line);
-        std::cout << "line " << line << std::endl;
-    }
-}
-
-void read_it(asio::ip::tcp::socket *sock) {
-    asio::async_read_until(*sock, b, "\n", handle_read);
-}
-
-int main(int ac, char *av[])
-{
-    asio::io_service io_service_object;
-    asio::ip::tcp::acceptor acceptor_server(
-        io_service_object, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 9999));
-    asio::ip::tcp::socket server_socket(io_service_object);
     asio::streambuf buf;
+    read_until(socket, buf, "\n");
+    string data = buffer_cast<const char*>(buf.data());
+    return data;
+}
 
-    acceptor_server.accept(server_socket); // TODO: use async accept
-    read_it(&server_socket);
+// Driver program to send data
+void sendData(tcp::socket& socket, const string& message)
+{
+    write(socket,
+          buffer(message + "\n"));
+}
+
+int main(int argc, char* argv[])
+{
+    io_service io_service;
+
+    // Initializing server acceptor
+    tcp::acceptor acceptor_server(io_service, tcp::endpoint(tcp::v4(), 9999));
+
+    // Creating socket object
+    tcp::socket server_socket(io_service);
+
+    // waiting for connection (could also use async_accept to do it asynchronously)
+    acceptor_server.accept(server_socket);
+
+    // Reading username (waiting for first msg)
+    string u_name = getData(server_socket);
+    // Removing "\n" from the username
+    u_name.pop_back();
+
+    // Replying with default message to initiate chat
+    string response, reply;
+    reply = "Hello " + u_name + "!";
+    cout << "Server: " << reply << endl;
+    sendData(server_socket, reply);
+
+    while (true) {
+
+        // Fetching response
+        response = getData(server_socket);
+        response.pop_back();
+
+        // Validating if the connection has to be closed
+        if (response == "exit") {
+            cout << u_name << " left!" << endl;
+            break;
+        }
+        cout << u_name << ": " << response << endl;
+
+        // Reading new message from input stream
+        cout << "Server" << ": ";
+        getline(cin, reply);
+        sendData(server_socket, reply);
+
+        if (reply == "exit")
+            break;
+    }
+    return 0;
 }
