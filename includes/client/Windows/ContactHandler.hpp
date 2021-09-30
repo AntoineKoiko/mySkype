112d248ee6d72c676dc0406394aac67e5e9e1c6e
AntoineKoiko <antoine.couacault@epitech.eu>
2021-09-30 17:53:51 +0200

    feat(ContactHandler): prepara class

--- includes/client/Windows/ContactHandler.hpp
+++ includes/client/Windows/ContactHandler.hpp
@@ -8,13 +8,44 @@
 #ifndef CONTACTHANDLER_HPP_
 #define CONTACTHANDLER_HPP_
 
+#include <vector>
+#include <string>
+
+class Contact
+{
+public:
+    Contact();
+    ~Contact();
+
+private:
+    std::string _username;
+};
+
+Contact::Contact(/* args */)
+{
+}
+
+Contact::~Contact()
+{
+}
+
 class ContactHandler
 {
 public:
     ContactHandler();
     ~ContactHandler();
 
+    // std::vector<Contact> getContactsList() const noexcept;
+    // std::vector<Contact> getContactRequests const noexcept:
+    // void acceptContactRequest(const std::string &username):
+    // void dismissContactRequest(const std::string &username):
+    // bool addContact(const std::string &username) const; //-> need to have NetworkAPI
+
 private:
+    // std::vector<Contact> _contacts;
+    // std::vector<Contact> _pendingContact;
+
+    //NetWorkApi _netAPI;
 };
 
 #endif /* !CONTACTHANDLER_HPP_ */
